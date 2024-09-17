1ab6644edc60f1ffde20f60939eb5c9df286c429
Valentin Voillet <valentin.voillet@gmail.com>
2024-09-17 13:57:26 +0200

    test

--- R/extract_CYTNUM_data.R
+++ R/extract_CYTNUM_data.R
@@ -21,51 +21,51 @@ extract_CYTNUM_data <- function(gs,
                                 cytokine_nodes)
 {
   ##-- Require
-  require(flowWorkspace)
+  #require(flowWorkspace)
   require(tidyverse)
-  require(data.table)
+  #require(data.table)
 
 
   ##-- Extraction
-  exprs.tmp <- lapply(gs, function(x)
+  exprs.tmp <- flowWorkspace::lapply(gs, function(x)
   {
-    cat("\t", pData(x) %>% rownames(), "\n")
+    cat("\t", flowWorkspace::pData(x) %>% rownames(), "\n")
 
     #- Get boolean positivity call for markers
     options(warn = 0)
-    marker_response <- try(lapply(c(parent_node, cytokine_nodes), function(mrkr){gh_pop_get_indices(x, mrkr)}))
+    marker_response <- try(lapply(c(parent_node, cytokine_nodes), function(mrkr){flowWorkspace::gh_pop_get_indices(x, mrkr)}))
     while(class(marker_response) == "try-error"){
-      marker_response <- try(lapply(c(parent_node, cytokine_nodes), function(mrkr){gh_pop_get_indices(x, mrkr)}))
+      marker_response <- try(lapply(c(parent_node, cytokine_nodes), function(mrkr){flowWorkspace::gh_pop_get_indices(x, mrkr)}))
     }
     names(marker_response) <- c(parent_node, cytokine_nodes)
-    marker_response <- bind_rows(marker_response)
+    marker_response <- dplyr::bind_rows(marker_response)
 
     #- data.table()
     dt.res <- marker_response %>%
-      data.table() %>%
-      mutate(FCS = rownames(pData(x))) %>%
-      mutate(BATCH = pData(x)$BATCH) %>%
-      mutate(PTID = pData(x)$PTID) %>%
-      mutate(STIM = pData(x)$STIM) %>%
-      mutate(VISITNO = pData(x)$VISITNO) %>%
-      mutate(RUNNUM = pData(x)$`Run Num`) %>%
-      mutate(SAMP_ORD = pData(x)$SAMP_ORD) %>%
-      mutate(REPLICATE = pData(x)$Replicate)
+      data.table::data.table() %>%
+      dplyr::mutate(FCS = rownames(flowWorkspace::pData(x))) %>%
+      dplyr::mutate(BATCH = flowWorkspace::pData(x)$BATCH) %>%
+      dplyr::mutate(PTID = flowWorkspace::pData(x)$PTID) %>%
+      dplyr::mutate(STIM = flowWorkspace::pData(x)$STIM) %>%
+      dplyr::mutate(VISITNO = flowWorkspace::pData(x)$VISITNO) %>%
+      dplyr::mutate(RUNNUM = flowWorkspace::pData(x)$`Run Num`) %>%
+      dplyr::mutate(SAMP_ORD = flowWorkspace::pData(x)$SAMP_ORD) %>%
+      dplyr::mutate(REPLICATE = flowWorkspace::pData(x)$Replicate)
     dt.res$CYTNUM <- apply(dt.res[, ..cytokine_nodes], 1, function(x) sum(x == TRUE))
 
     #- Output
     dt.output <- dt.res %>%
-      filter(get({{parent_node}}) == TRUE) %>%
-      group_by(FCS, BATCH, PTID, STIM, VISITNO, RUNNUM, REPLICATE, SAMP_ORD) %>%
-      mutate(NSUB = n()) %>%
-      mutate(boolean_CYTNUM = factor(x = ifelse(CYTNUM >= 1, TRUE, FALSE), levels = c(TRUE, FALSE))) %>%
-      group_by(BATCH, PTID, STIM, VISITNO, RUNNUM, REPLICATE, SAMP_ORD, NSUB, boolean_CYTNUM, .drop = FALSE) %>%
-      summarize(CYTNUM = n())
+      dplyr::filter(get({{parent_node}}) == TRUE) %>%
+      dplyr::group_by(FCS, BATCH, PTID, STIM, VISITNO, RUNNUM, REPLICATE, SAMP_ORD) %>%
+      dplyr::mutate(NSUB = n()) %>%
+      dplyr::mutate(boolean_CYTNUM = factor(x = ifelse(CYTNUM >= 1, TRUE, FALSE), levels = c(TRUE, FALSE))) %>%
+      dplyr::group_by(BATCH, PTID, STIM, VISITNO, RUNNUM, REPLICATE, SAMP_ORD, NSUB, boolean_CYTNUM, .drop = FALSE) %>%
+      dplyr::summarize(CYTNUM = n())
     dt.output %>% return()
   })
 
 
   ##-- Output
-  bind_rows(exprs.tmp) %>% return()
+  dplyr::bind_rows(exprs.tmp) %>% return()
 }
 
